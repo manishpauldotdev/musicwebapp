37211f889929c1f219e1b51e342526e56995b9af
Shashank <shashank@kali>
2024-07-30 21:41:07 +0530

    v2

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,7 @@
+import 'dart:ui';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,10 +11,13 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
   late AudioPlayer audioPlayer = AudioPlayer();
   bool isPlaying = false;
   bool hasStartedPlaying = false;
+  late AnimationController _controller;
+  late Animation<Offset> _animation;
 
   void startAudio() async {
     await audioPlayer.play(AssetSource('audio/upbeat-music.mp3'));
@@ -37,6 +42,26 @@ class _HomePageState extends State<HomePage> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 10),
+      vsync: this,
+    )..repeat(reverse: true);
+    _animation = Tween<Offset>(
+      begin: const Offset(1, 0),
+      end: const Offset(-1, 0),
+    ).animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    audioPlayer.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
@@ -51,7 +76,8 @@ class _HomePageState extends State<HomePage> {
                     width: double.maxFinite,
                     height: 700,
                     child: Center(
-                      child: Text('This music player is only designed for mobile view'),
+                      child: Text(
+                          'This music player is only designed for mobile view'),
                     ),
                   );
                 } else {
@@ -61,7 +87,15 @@ class _HomePageState extends State<HomePage> {
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
                           Text('Music'),
-                          Text('237 Online'),
+                          Row(
+                            children: [
+                              Text(
+                                '237',
+                                style: TextStyle(color: Colors.white70),
+                              ),
+                              Text(' Online'),
+                            ],
+                          ),
                         ],
                       ),
                       const SizedBox(height: 16),
@@ -77,45 +111,98 @@ class _HomePageState extends State<HomePage> {
                           foregroundColor: Colors.white,
                         ),
                         onPressed: () {},
-                        child: const Text('Back to Plinko'),
-                      ),
-                      const SizedBox(height: 16),
-                      const Text(
-                        'Tru Tones - Dancing (feat. Rog)',
-                        style: TextStyle(fontSize: 22),
-                      ),
-                      const Text(
-                        'Seth Pentalony',
-                        style: TextStyle(fontSize: 18),
-                      ),
-                      const SizedBox(height: 16),
-                      Container(
-                        height: 350,
-                        width: double.maxFinite,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          color: Colors.black54,
+                        child: Text(
+                          'Back to Plinko',
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleMedium
+                              ?.copyWith(
+                                  fontSize: 20, fontWeight: FontWeight.w600),
                         ),
                       ),
                       const SizedBox(height: 16),
+                      ClipRect(
+                        child: SizedBox(
+                          height: 34,
+                          child: SlideTransition(
+                            position: _animation,
+                            child: Text(
+                              'Tru Tones - Dancing (feat. Rog) Over The Moonlight',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleLarge!
+                                  .copyWith(
+                                    color: Colors.white,
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 2),
+                      Row(
+                        children: [
+                          Text(
+                            'Seth Pentalony',
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleMedium!
+                                .copyWith(
+                                    color: Colors.white54,
+                                    fontWeight: FontWeight.w700),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 16),
+                      Stack(
+                        children: [
+                          Container(
+                            height: 320,
+                            width: double.maxFinite,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              color: Colors.white38.withOpacity(0.1),
+                            ),
+                          ),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(15),
+                            child: BackdropFilter(
+                              filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                              child: Container(
+                                height: 320,
+                                width: double.maxFinite,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                  color: Colors.black.withOpacity(0.1),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 32),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Container(
-                            height: 56,
-                            width: 100,
-                            decoration: const BoxDecoration(
-                              borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(50),
-                                bottomLeft: Radius.circular(50),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(8),
+                            child: Container(
+                              height: 56,
+                              width: 100,
+                              decoration: const BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(50),
+                                  bottomLeft: Radius.circular(50),
+                                ),
+                                color: Colors.black87,
                               ),
-                              color: Colors.black87,
-                            ),
-                            child: const Center(
-                              child: Icon(
-                                Icons.skip_previous_rounded,
-                                color: Colors.white,
-                                size: 30,
+                              child: const Center(
+                                child: Icon(
+                                  Icons.skip_previous_rounded,
+                                  color: Colors.white,
+                                  size: 30,
+                                ),
                               ),
                             ),
                           ),
@@ -128,37 +215,48 @@ class _HomePageState extends State<HomePage> {
                                 togglePlayingStatus();
                               }
                             },
-                            child: Container(
-                              height: 56,
-                              width: 100,
-                              decoration: const BoxDecoration(
-                                color: Colors.purple,
-                              ),
-                              child: Center(
-                                child: Icon(
-                                  isPlaying ? Icons.pause_rounded : Icons.play_arrow_rounded,
-                                  color: Colors.white,
-                                  size: 30,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(8),
+                              child: Container(
+                                height: 56,
+                                width: 100,
+                                decoration: BoxDecoration(
+                                    color: Colors.purple[200],
+                                    gradient: LinearGradient(colors: [
+                                      Colors.purple,
+                                      Colors.pinkAccent,
+                                    ])),
+                                child: Center(
+                                  child: Icon(
+                                    isPlaying
+                                        ? Icons.pause_rounded
+                                        : Icons.play_arrow_rounded,
+                                    color: Colors.white,
+                                    size: 30,
+                                  ),
                                 ),
                               ),
                             ),
                           ),
                           const SizedBox(width: 16),
-                          Container(
-                            height: 56,
-                            width: 100,
-                            decoration: const BoxDecoration(
-                              borderRadius: BorderRadius.only(
-                                topRight: Radius.circular(50),
-                                bottomRight: Radius.circular(50),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(8),
+                            child: Container(
+                              height: 56,
+                              width: 100,
+                              decoration: const BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                                  topRight: Radius.circular(50),
+                                  bottomRight: Radius.circular(50),
+                                ),
+                                color: Colors.black87,
                               ),
-                              color: Colors.black87,
-                            ),
-                            child: const Center(
-                              child: Icon(
-                                Icons.skip_next_rounded,
-                                color: Colors.white,
-                                size: 30,
+                              child: const Center(
+                                child: Icon(
+                                  Icons.skip_next_rounded,
+                                  color: Colors.white,
+                                  size: 30,
+                                ),
                               ),
                             ),
                           ),
@@ -168,15 +266,21 @@ class _HomePageState extends State<HomePage> {
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          const Icon(Icons.volume_mute),
+                          Icon(
+                            Icons.volume_mute,
+                            color: Colors.purple[100],
+                          ),
                           const SizedBox(width: 8),
                           Container(
                             height: 6,
                             width: 150,
-                            decoration: const BoxDecoration(color: Colors.black87),
+                            decoration: BoxDecoration(
+                              color: Colors.purple[300],
+                              borderRadius: BorderRadius.circular(3),
+                            ),
                           ),
                           const SizedBox(width: 8),
-                          const Icon(Icons.volume_up),
+                          Icon(Icons.volume_up, color: Colors.purple[100]),
                         ],
                       ),
                     ],
--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,84 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: AppTheme.darkTheme,
       debugShowCheckedModeBanner: false,
       home: HomePage(),
+    );
+  }
+}
+
+class AppTheme {
+  static ThemeData get darkTheme {
+    return ThemeData(
+      brightness: Brightness.dark,
+      scaffoldBackgroundColor: Color(0xFF121212), // Dark background color
+      colorScheme: ColorScheme.dark(
+        primary: Colors.deepPurple,
+        secondary: Colors.deepPurpleAccent,
+        background: Color(0xFF121212),
+        surface: Color(0xFF1E1E1E),
+        onPrimary: Colors.white,
+        onSecondary: Colors.white,
+        onBackground: Colors.white,
+        onSurface: Colors.white,
+      ),
+      textTheme: TextTheme(
+        headlineLarge: TextStyle(
+          fontFamily: 'InterVariable',
+          fontWeight: FontWeight.bold,
+          fontSize: 32,
+          color: Colors.white,
+        ),
+        headlineMedium: TextStyle(
+          fontFamily: 'InterVariable',
+          fontWeight: FontWeight.w600,
+          fontSize: 28,
+          color: Colors.white70,
+        ),
+        headlineSmall: TextStyle(
+          fontFamily: 'InterVariable',
+          fontWeight: FontWeight.w500,
+          fontSize: 24,
+          color: Colors.white70,
+        ),
+        bodyLarge: TextStyle(
+          fontFamily: 'InterVariable',
+          fontSize: 16,
+          color: Colors.white70,
+        ),
+        bodyMedium: TextStyle(
+          fontFamily: 'InterVariable',
+          fontSize: 14,
+          color: Colors.white60,
+        ),
+        labelLarge: TextStyle(
+          fontFamily: 'InterVariable',
+          fontSize: 16,
+          color: Colors.white70,
+        ),
+        labelMedium: TextStyle(
+          fontFamily: 'InterVariable',
+          fontSize: 14,
+          color: Colors.white60,
+        ),
+        titleMedium: TextStyle(
+          fontFamily: 'InterVariable',
+          fontSize: 14,
+          color: Colors.white,
+        ),
+        titleSmall: TextStyle(
+          fontFamily: 'InterVariable',
+          fontSize: 12,
+          color: Colors.white60,
+        ),
+        labelSmall: TextStyle(
+          fontFamily: 'InterVariable',
+          fontSize: 10,
+          color: Colors.white60,
+        ),
+      ),
     );
   }
 }
