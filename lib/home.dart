842907bad5cc96a8b640698e06678c487653daf2
Manish Paul <manishpaul.dev@gmail.com>
2024-07-30 13:13:27 +0530

    mobile first design

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -17,98 +18,114 @@ class _HomePageState extends State<HomePage> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('Music'),
-                  Text('237 Online'),
-                ],
-              ),
-              const SizedBox(height: 16),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  minimumSize: const Size(double.maxFinite, 56),
-                  shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(12),
+              ResponsiveBuilder(builder: (context, deviceSize) {
+                if (deviceSize.isTablet || deviceSize.isDesktop) {
+                  return const SizedBox(
+                    width: double.maxFinite,
+                    height: 700,
+                    child: Center(
+                      child: Text('This music player is only designed for mobile view'),
                     ),
-                  ),
-                  backgroundColor: Colors.purple,
-                  foregroundColor: Colors.white,
-                ),
-                onPressed: () {},
-                child: const Text('Back to Plinko'),
-              ),
-              const SizedBox(height: 16),
-              const Text(
-                'Tru Tones - Dancing (feat. Rog)',
-                style: TextStyle(fontSize: 22),
-              ),
-              const Text(
-                'Seth Pentalony',
-                style: TextStyle(fontSize: 18),
-              ),
-              const SizedBox(height: 16),
-              Container(
-                height: 350,
-                width: double.maxFinite,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  color: Colors.black54,
-                ),
-              ),
-              const SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: 56,
-                    width: 100,
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(50),
-                        bottomLeft: Radius.circular(50),
+                  );
+                } else {
+                  return Column(
+                    children: [
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Music'),
+                          Text('237 Online'),
+                        ],
                       ),
-                      color: Colors.black45,
-                    ),
-                  ),
-                  const SizedBox(width: 16),
-                  Container(
-                    height: 56,
-                    width: 100,
-                    decoration: const BoxDecoration(
-                      color: Colors.black45,
-                    ),
-                  ),
-                  const SizedBox(width: 16),
-                  Container(
-                    height: 56,
-                    width: 100,
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(50),
-                        bottomRight: Radius.circular(50),
+                      const SizedBox(height: 16),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size(double.maxFinite, 56),
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(12),
+                            ),
+                          ),
+                          backgroundColor: Colors.purple,
+                          foregroundColor: Colors.white,
+                        ),
+                        onPressed: () {},
+                        child: const Text('Back to Plinko'),
                       ),
-                      color: Colors.black45,
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Icon(Icons.volume_mute),
-                  const SizedBox(width: 8),
-                  Container(
-                    height: 6,
-                    width: 150,
-                    decoration: const BoxDecoration(color: Colors.black87),
-                  ),
-                  const SizedBox(width: 8),
-                  const Icon(Icons.volume_up),
-                ],
-              ),
+                      const SizedBox(height: 16),
+                      const Text(
+                        'Tru Tones - Dancing (feat. Rog)',
+                        style: TextStyle(fontSize: 22),
+                      ),
+                      const Text(
+                        'Seth Pentalony',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      const SizedBox(height: 16),
+                      Container(
+                        height: 350,
+                        width: double.maxFinite,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: Colors.black54,
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            height: 56,
+                            width: 100,
+                            decoration: const BoxDecoration(
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(50),
+                                bottomLeft: Radius.circular(50),
+                              ),
+                              color: Colors.black45,
+                            ),
+                          ),
+                          const SizedBox(width: 16),
+                          Container(
+                            height: 56,
+                            width: 100,
+                            decoration: const BoxDecoration(
+                              color: Colors.black45,
+                            ),
+                          ),
+                          const SizedBox(width: 16),
+                          Container(
+                            height: 56,
+                            width: 100,
+                            decoration: const BoxDecoration(
+                              borderRadius: BorderRadius.only(
+                                topRight: Radius.circular(50),
+                                bottomRight: Radius.circular(50),
+                              ),
+                              color: Colors.black45,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 16),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Icon(Icons.volume_mute),
+                          const SizedBox(width: 8),
+                          Container(
+                            height: 6,
+                            width: 150,
+                            decoration: const BoxDecoration(color: Colors.black87),
+                          ),
+                          const SizedBox(width: 8),
+                          const Icon(Icons.volume_up),
+                        ],
+                      ),
+                    ],
+                  );
+                }
+              }),
             ],
           ),
         ),
